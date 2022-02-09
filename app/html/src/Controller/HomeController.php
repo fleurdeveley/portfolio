89c4bc47a2938970a4bfa3d6b903ab50fa24e072
fleur <fleurdeveley@gmail.com>
2022-02-09 09:01:45 +0100

    fixed home

--- app/html/src/Controller/HomeController.php
+++ app/html/src/Controller/HomeController.php
@@ -2,14 +2,10 @@
 
 namespace App\Controller;
 
-use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class HomeController extends AbstractController
 {
-    /**
-     * @Route("/", name="homepage")
-     */
     public function index()
     {
         return $this->render('home.html.twig');
