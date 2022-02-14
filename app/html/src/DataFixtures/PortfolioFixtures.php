09e77255f0d9a7a032e01d4677afb49a3fc31280
fleur <fleurdeveley@gmail.com>
2022-02-14 15:59:03 +0100

    database and view portfolio

--- app/html/src/DataFixtures/PortfolioFixtures.php
+++ app/html/src/DataFixtures/PortfolioFixtures.php
@@ -2,15 +2,16 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\Project;
 use App\Entity\Skill;
 use DateTimeImmutable;
 use Doctrine\Persistence\ObjectManager;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 
-class SkillFixtures extends Fixture
+class PortfolioFixtures extends Fixture
 {
     public function load(ObjectManager $manager): void
-    {
+    {        
         $skills = [
             'symfony',
             'composer',
@@ -39,6 +40,19 @@ class SkillFixtures extends Fixture
             $manager->persist($skill);
         }
 
+        $project = new Project;
+
+        $project->setName('SnowTricks')
+            ->setDescription('Site communautaire autour du snowboard')
+            ->setImage('snowtricks.png')
+            ->setUrlSite('https://projet6.fleurdeveley.fr/')
+            ->setUrlGithub('https://github.com/fleurdeveley/OC_P6_SnowTricks')
+            ->setActivated(1)
+            ->setCreatedAt(new DateTimeImmutable())
+            ->setUpdatedAt(new DateTimeImmutable());
+
+        $manager->persist($project);
+        
         $manager->flush();
     }
 }
