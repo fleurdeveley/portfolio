3a0179f1efcb5cbba340c8c31c76cb56ce524162
fleur <fleurdeveley@gmail.com>
2022-03-01 10:59:10 +0100

    set up an administrator dashboard

--- app/html/src/Controller/PortfolioController.php
+++ app/html/src/Controller/PortfolioController.php
@@ -8,9 +8,9 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class PortfolioController extends AbstractController
 {
-    public function list(ProjectRepository $projectRepository, SkillRepository $skillRepository)
+    public function index(ProjectRepository $projectRepository, SkillRepository $skillRepository)
     {
-        return $this->render('portfolio.html.twig', [
+        return $this->render('/portfolio.html.twig', [
             'projects' => $projectRepository->findByActivated(),
             'skills' => $skillRepository->findAll()
         ]);
