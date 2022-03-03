26f804c71dabe0b73ee8cd5671c743d7dae8cc7c
fleur <fleurdeveley@gmail.com>
2022-03-03 14:49:15 +0100

    ContactType

---
+++ app/html/src/Form/ContactType.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TelType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+
+class ContactType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('fullName', TextType::class, [
+                'label' => 'Prénom et Nom',
+                'attr' => ['placeholder' => 'Vorname '],
+                'attr' => ['class' => 'form-control mb-3'],
+            ])
+            ->add('email', EmailType::class, [
+                'label' => 'Email',
+                'attr' => ['class' => 'form-control mb-3'],
+            ])
+            ->add('phone', TelType::class, [
+                'label' => 'Portable',
+                'attr' => ['class' => 'form-control mb-3'],
+            ])
+            ->add('message', TextareaType::class, [
+                'label' => 'Message',
+                'attr' => ['rows' => 6, 'class' => 'form-control mb-3'],
+            ])
+            ->add('save', SubmitType::class, [
+                'label' => 'Envoyer',
+                'attr' => ['class' => 'btn form-control mb-3'],
+            ]);
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            // Configure your form options here
+        ]);
+    }
+}
